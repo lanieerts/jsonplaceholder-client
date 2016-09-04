dfd0d940fc6508f053b3431792e4971cd367d8b4
lanieerts <lanieerts@gmail.com>
2016-09-04 21:14:15 +0900

    Add sidebar under the title.

--- src/containers/About/About.js
+++ src/containers/About/About.js
@@ -1,16 +1,66 @@
 import React from 'react';
 import { Link } from 'react-router';
-import { Button } from 'react-bootstrap';
+import Sidebar from 'react-sidebar';
 
 class About extends React.Component {
   render() {
+    let sidebarStyle = {
+      root: {
+        position: 'relative',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        overflow: 'hidden',
+      },
+      sidebar: {
+        zIndex: 2,
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        transition: 'transform .3s ease-out',
+        WebkitTransition: '-webkit-transform .3s ease-out',
+        willChange: 'transform',
+        overflowY: 'hidden',
+        marginRight: 50,
+      },
+      content: {
+        position: 'relative',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        overflow: 'auto',
+        transition: 'left .3s ease-out, right .3s ease-out',
+      },
+      overlay: {
+        zIndex: 1,
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        opacity: 0,
+        visibility: 'hidden',
+        transition: 'opacity .3s ease-out',
+        backgroundColor: 'rgba(0,0,0,.3)',
+      },
+      dragHandle: {
+        zIndex: 1,
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+      }
+    };
+
     return (
       <div>
         <h1 className="text-center"> ABOUT PAGE </h1>
-        <Link to="/about/issues">
-          <Button bsStyle="link"> ISSUES </Button>
-        </Link>
-        {this.props.children}
+        <Sidebar sidebar={<b> <Link to="/about/issues"> Sidebar Content </Link> </b>}
+                 docked={true} shadow={false} styles={sidebarStyle}>
+          TESTINGGGGGGGGGGGG
+          {this.props.children}
+        </Sidebar>
       </div>
     )
   }
